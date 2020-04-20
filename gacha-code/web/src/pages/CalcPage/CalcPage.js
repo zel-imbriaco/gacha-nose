2ff4ff48c33504a00d95fa749d1dc8c59ed99978
Vaeliwen <zaman@imbriaco.com>
2020-04-20 12:54:24 -0400

    Created calculator page's calculator.

--- gacha-code/web/src/pages/CalcPage/CalcPage.js
+++ gacha-code/web/src/pages/CalcPage/CalcPage.js
@@ -1,9 +1,29 @@
 import NavbarLayout from "src/layouts/NavbarLayout/NavbarLayout"
+import { useState, useEffect } from "react"
 
 const CalcPage = () => {
+  const [calcstate, setCalcstate] = useState({desire:0, draws:0, chance:0.00, compoundedChance:''})
+
+  const probability = (100 * (1 - (Math.pow((1-(calcstate.chance/100)) , calcstate.draws / calcstate.desire)))).toFixed(2);
+
+  useEffect(() => {
+    const compoundedChance = probability;
+    setCalcstate({...calcstate, compoundedChance: compoundedChance})}
+    , [])
+
+
+    const changeHandler = (e) => {
+    setCalcstate({...calcstate, [e.target.name]: e.target.value})
+  }
+
   return (
       <NavbarLayout>
-        Calculator Page
+        <form>
+          <input name="desire" placeholder='Desired # of results' onChange={changeHandler}></input>
+          <input name="draws" placeholder='# of total draws' onChange={changeHandler}></input>
+          <input name="chance" placeholder='% chance of desired result' onChange={changeHandler}></input>
+        </form>
+        <h1>{["NaN", "Infinity", "-Infinity"].includes(probability.toString()) ? "Input numbers in all three fields to find out your odds!" : probability + "%"}</h1>      
       </NavbarLayout>
   )
 }
--- gacha-code/web/src/pages/HomePage/HomePage.js
+++ gacha-code/web/src/pages/HomePage/HomePage.js
@@ -1,8 +1,9 @@
+import NavbarLayout from '../../layouts/NavbarLayout'
+
 const HomePage = () => {
   return (
     <NavbarLayout>
-      <h1>HomePage</h1>
-      <p>Find me in ./web\src\pages\HomePage\HomePage.js</p>
+      Home Page.
     </NavbarLayout>
   )
 }
